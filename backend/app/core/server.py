6ae457da2bf98501027a882489b5c29622c2ea50
PLINTUS <Xpaul6@gmail.com>
2024-04-01 13:42:53 +0300

    CORS fix

--- backend/app/core/server.py
+++ backend/app/core/server.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, APIRouter
 from app.core.routers import routers
 from typing import Tuple
+from fastapi.middleware.cors import CORSMiddleware
+
+ORIGINS = [
+    '*'
+]
 
 
 class Server:
@@ -10,11 +15,20 @@ class Server:
     def __init__(self, app: FastAPI) -> None:
         self.app = app
         self.register_routers(routers)
-
+        self.add_cors(ORIGINS)
 
     def get_app(self) -> FastAPI:
         return self.app
-    
+
+    def add_cors(self, origins: Tuple[str]):
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=['*'],
+            allow_headers=['*']
+        )
+
     def register_routers(self, routers: Tuple[APIRouter]):
         for router in routers:
             self.app.include_router(router = router)
