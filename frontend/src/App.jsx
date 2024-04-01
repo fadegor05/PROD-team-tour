66e9a5f12207e1d1c742f40ecc6218b17f50388c
PLINTUS <Xpaul6@gmail.com>
2024-04-01 10:09:50 +0300

    form layout + routing
    form layout + blank auth + routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { redirect, useNavigate } from 'react-router-dom'
+
+const TEST_PHONE_NUMBER = '+79527774242'
 
 export default function App() {
+  const navigate = useNavigate()
+  const [login, setLogin] = useState(TEST_PHONE_NUMBER)
+
+  function auth() {
+    if (login == TEST_PHONE_NUMBER) { 
+      navigate('/form')
+    }
+  }
+
+  useEffect(auth, [login])
   return (
-    <>
-      <h1>MeetSync</h1>
-      <p>Hello world</p>
-    </>
+    <div>App</div>
   )
 }
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+import styles from './Form.module.css'
+
+export default function Form() {
+  const [status, setStatus] = useState('new')
+
+  return (
+    <div className={styles.card}>
+
+      {status == 'new' &&
+      <>
+      <h2 className={styles.title}>Назначить встречу</h2>
+      <h3 className={styles.subtitle}>Дата</h3>
+      <input type="date" className={styles.dateinput}/>
+      <h3 className={styles.subtitle}>Место</h3>
+      <input type="text" placeholder="Место встречи" className={styles.textinput}/>
+      <button className={styles.confirmbutton} onClick={() => setStatus('selectTime')}>Выбрать</button>
+      </>
+      }
+
+      {status == 'selectTime' &&
+      <>
+      <h2 className={styles.title}>Выберите время</h2>
+      <div className={styles.listitem}>
+        <div className={styles.timeblock}><p className={styles.infofield}>10:00-11:00</p></div>
+        <button className={styles.selectbutton} onClick={() => setStatus('confirm')}>Выбрать</button>
+      </div>
+      <div className={styles.listitem}>
+        <div className={styles.timeblock}><p className={styles.infofield}>11:00-12:00</p></div>
+        <button className={styles.selectbutton} onClick={() => setStatus('confirm')}>Выбрать</button>
+      </div>
+      </>
+      }
+
+      {status == 'confirm' &&
+      <>
+      <h2 className={styles.title}>Подтверждение</h2>
+      <h3 className={styles.subtitle}>Время и место</h3>
+      <div className={styles.infobox}>
+        <p className={styles.infofield}>5 апреля, 14:00</p>
+        <p className={styles.infofield}>Ул. Московская д. 5 оф. 4</p>
+      </div>
+      <h3 className={styles.subtitle}>Представитель</h3>
+      <div className={styles.infobox}>
+        <p className={styles.infofield}>Зубенко Михаил Петрович</p>
+        <p className={styles.infofield}>+794356275782</p>
+      </div>
+      <h3 className={styles.subtitle}>Пакет документов</h3>
+      <div className={styles.infobox}>
+        <p className={styles.infofield}>Паспорт</p>
+        <p className={styles.infofield}>Регистрация юр. лица</p>
+      </div>
+      <h4 className={styles.infofield}></h4>
+      <button className={styles.confirmbutton} onClick={() => setStatus('new')}>Подтвердить</button>
+      </>
+      }
+
+    </div>
+  )
+}
