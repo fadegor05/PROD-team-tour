34f09530da49d08de49ed4134221f4c0162f1dca
PLINTUS <Xpaul6@gmail.com>
2024-04-01 17:13:57 +0300

    edit meeting

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -8,13 +8,14 @@ import Meetings from './components/Meetings'
 
 export default function Router() {
   const [userInfo, setUserInfo] = useState({})
+  const [currentMeeting, setCurrentMeeting] = useState({})
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App updateUserInfo={(userInfo) => setUserInfo(userInfo)}/>}/>
-        <Route path='/form' element={<Form userInfo={userInfo}/>} />
-        <Route path='/meetings' element={<Meetings userInfo={userInfo}/>} />
+        <Route path='/form' element={<Form userInfo={userInfo} currentMeeting={currentMeeting} updateCurrentMeeting={(meeting) => setCurrentMeeting(meeting)}/>} />
+        <Route path='/meetings' element={<Meetings userInfo={userInfo} updateCurrentMeeting={(meeting) => setCurrentMeeting(meeting)}/>}/>
     </Routes>
     </BrowserRouter>
   )
