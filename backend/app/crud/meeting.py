060f0bed5516aca3b3c8b8471d9c72686cde52a3
fadegor05 <fad.egor05@gmail.com>
2024-04-01 17:04:13 +0300

    updated meeting crud, added get meetings by date and agent

--- backend/app/crud/meeting.py
+++ backend/app/crud/meeting.py
@@ -1,9 +1,20 @@
 import datetime
+from typing import List
 
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User, Agent, Meeting
+
+
+async def get_meetings_by_date_and_agent(session: AsyncSession, date: datetime.datetime, agent: Agent) -> List[Meeting]:
+    min_datetime = datetime.datetime(date.year, date.month, date.day, 0, 0, 0)
+    max_datetime = datetime.datetime(date.year, date.month, date.day, 23, 59, 59)
+    stmt = select(Meeting).where(and_(Meeting.start_datetime >= min_datetime, Meeting.end_datetime <= max_datetime,
+                                      Meeting.agent_id == agent.id, Meeting.status == 'confirmed'))
+    result = await session.scalars(stmt)
+    meetings = result.all()
+    return meetings
 
 
 async def create_meeting(session: AsyncSession, user: User, agent: Agent,
