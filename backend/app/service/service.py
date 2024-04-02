dce0ce53fcfe10a8d6dc8b638509f5d2504d9d4a
fadegor05 <fad.egor05@gmail.com>
2024-04-02 10:25:58 +0300

    get route duration fixed if same place

--- backend/app/service/service.py
+++ backend/app/service/service.py
@@ -5,4 +5,8 @@ from app.service.api import request_ors_api
 
 async def get_route_duration(coordinates: List[Tuple[float, float]]) -> int:
     response = await request_ors_api(coordinates)
-    return round(response['routes'][0]['summary']['duration'] / 60)
+    try:
+        duration = round(response['routes'][0]['summary']['duration'] / 60)
+    except:
+        duration = 1
+    return duration
