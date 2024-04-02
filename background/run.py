0805469d2b55b387a9e3010608d3d9d1b410934a
fadegor05 <fad.egor05@gmail.com>
2024-04-02 23:52:03 +0300

    created main loop

--- background/run.py
+++ background/run.py
@@ -1,19 +1,22 @@
+import time
+
 import psycopg2
 
-from config import POSTGRES_URL
+from config import POSTGRES_URL, TIME
 from services.meetings import complete_meetings_handler
 from services.notifications import notify_users_handler
 
 
 def main():
-    conn = psycopg2.connect(POSTGRES_URL)
-    cur = conn.cursor()
+    while True:
+        conn = psycopg2.connect(POSTGRES_URL)
+        cur = conn.cursor()
+        complete_meetings_handler(conn, cur)
+        notify_users_handler(conn, cur)
 
-    complete_meetings_handler(conn, cur)
-    notify_users_handler(conn, cur)
-
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+        time.sleep(TIME)
 
 
 if __name__ == '__main__':
