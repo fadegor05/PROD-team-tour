d1197e9ca9e0c97881bdf34ea3881293ee7fb9a4
fadegor05 <fad.egor05@gmail.com>
2024-04-02 17:41:35 +0300

    Revert "changed from datetime.datetime to datetime"
    This reverts commit fb49340b4c3146d7fb83e35594fcad9ae3f399ad.

--- backend/app/crud/meeting.py
+++ backend/app/crud/meeting.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+import datetime
 from typing import List
 
 from sqlalchemy import select, and_
@@ -7,9 +7,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models import User, Agent, Meeting
 
 
-async def get_meetings_by_date_and_agent(session: AsyncSession, date: datetime, agent: Agent) -> List[Meeting]:
-    min_datetime = datetime(date.year, date.month, date.day, 0, 0, 0)
-    max_datetime = datetime(date.year, date.month, date.day, 23, 59, 59)
+async def get_meetings_by_date_and_agent(session: AsyncSession, date: datetime.datetime, agent: Agent) -> List[Meeting]:
+    min_datetime = datetime.datetime(date.year, date.month, date.day, 0, 0, 0)
+    max_datetime = datetime.datetime(date.year, date.month, date.day, 23, 59, 59)
     stmt = select(Meeting).where(and_(Meeting.start_datetime >= min_datetime, Meeting.end_datetime <= max_datetime,
                                       Meeting.agent_id == agent.id,
                                       Meeting.status == 'confirmed')).order_by(Meeting.start_datetime)
@@ -19,7 +19,7 @@ async def get_meetings_by_date_and_agent(session: AsyncSession, date: datetime, 
 
 
 async def create_meeting(session: AsyncSession, user: User, agent: Agent,
-                         start_datetime: datetime, end_datetime: datetime,
+                         start_datetime: datetime.datetime, end_datetime: datetime.datetime,
                          place: str, lon: float, lat: float) -> Meeting:
     meeting = Meeting(user=user, agent=agent, start_datetime=start_datetime,
                       end_datetime=end_datetime, place=place, lon=lon, lat=lat)
