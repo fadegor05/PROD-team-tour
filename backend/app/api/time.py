e2d78ac901420bb5b2ca28cf0ce1fc3c99d61f4d
fadegor05 <fad.egor05@gmail.com>
2024-04-02 08:49:56 +0300

    added creating datetime dict with available agents id

--- backend/app/api/time.py
+++ backend/app/api/time.py
@@ -8,7 +8,7 @@ import geocoder
 from fastapi import HTTPException
 
 from app.api.router import api_router
-from app.core.utils import generate_datetime_list
+from app.core.utils import generate_datetime_dict, generate_datetime_list
 from app.crud.agent import get_agents_all
 from app.crud.meeting import get_meetings_by_date_and_agent
 from app.database import async_session
@@ -21,15 +21,15 @@ async def available_time_handler(request_schema: AvailableTimePost) -> List[Avai
     async with async_session() as session:
         date = datetime.fromisoformat(request_schema.date)
         geo = geocoder.osm(request_schema.place).json
-        lon = geo['lng']
-        lat = geo['lat']
+        lon = geo.get('lng')
+        lat = geo.get('lat')
         if not lon or not lat:
             raise HTTPException(status_code=404, detail='place not found')
         agents = await get_agents_all(session)
         datetime_list = await generate_datetime_list(date)
+        available_time = await generate_datetime_dict(date)
         for agent in agents:
             meetings = await get_meetings_by_date_and_agent(session, date, agent)
-            available_time = {}
             agent_datetime_list = deepcopy(datetime_list)
             for n, meeting in enumerate(meetings):
                 duration = await get_route_duration([(meeting.lon, meeting.lat), (lon, lat)])
@@ -42,7 +42,7 @@ async def available_time_handler(request_schema: AvailableTimePost) -> List[Avai
                         agent_datetime_list.remove(current_datetime)
                     current_datetime += timedelta(minutes=15)
             for agent_datetime in agent_datetime_list:
-                if agent_datetime not in available_time:
-                    available_time[agent_datetime] = True
+                if agent_datetime in available_time:
+                    available_time[agent_datetime].append(agent.id)
     response = [AvailableTimePostResponse(start_datetime=time.isoformat()) for time in list(available_time)]
     return response
