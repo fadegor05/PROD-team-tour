1d607378700978dbf00975d5ac80f7702a106d95
fadegor05 <fad.egor05@gmail.com>
2024-04-02 23:53:34 +0300

    deleted easy schedule integration

--- backend/app/api/utils.py
+++ backend/app/api/utils.py
@@ -1,19 +1,10 @@
-from easyschedule import EasyScheduler
-
 from app.api.router import api_router
 from app.core.utils import db_fill
 from app.database import db_init
 
-scheduler = EasyScheduler()
-
-
-@scheduler(schedule='15 * * * *')
-async def main_schedule():
-    ...
 
 
 @api_router.on_event('startup')
 async def on_startup_db_init():
     await db_init()
     await db_fill()
-    await scheduler.start()
