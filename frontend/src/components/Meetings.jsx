edefb25930ec5b02fba02a849c448efc2a3ea0df
PLINTUS <Xpaul6@gmail.com>
2024-04-02 12:41:50 +0300

    Update Meetings.jsx

--- frontend/src/components/Meetings.jsx
+++ frontend/src/components/Meetings.jsx
@@ -35,7 +35,7 @@ export default function Meetings({ userInfo, updateCurrentMeeting }) {
 
   function cancelMeeting(id, el) {
     if (confirm(`${formatDate(el.start_datetime)}\n${el.place}\nОтменить встречу?`)) {
-      fetch('http://localhost:8000/api/meeting', {
+      fetch('/api/meeting', {
       method: 'PATCH',
       body: JSON.stringify({
         meeting_id: id,
