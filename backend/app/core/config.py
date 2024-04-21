ce0cc6a7cebd0eac36e5d139418e9cc989a18231
fadegor05 <fad.egor05@gmail.com>
2024-04-21 22:26:03 +0400

    prepared for publishing

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,2 +1,2 @@
 SQLALCHEMY_URL = 'postgresql+asyncpg://someuser:12345678@postgres/postgres'
-ORS_KEY = '5b3ce3597851110001cf6248a50c663c52494e928442c370f8f9da00'
+ORS_KEY = 'ORS_KEY'
--- background/config.py
+++ background/config.py
@@ -1,3 +1,3 @@
 POSTGRES_URL = 'postgresql://someuser:12345678@postgres/postgres'
-SMS_KEY = 'xpaul6@gmail.com:38RApr1UuYnQmnm-fTt7Vf28q-EUPTgH'
+SMS_KEY = 'SMS_KEY'
 TIME = 900
